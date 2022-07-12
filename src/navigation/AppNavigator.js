e2f4da87dab46db817dc40fce2cb438cc34c0840
Sooryasanand <sooryanarayanan.com@gmail.com>
2022-07-12 12:54:22 +1000

    Made it a better boiler plate

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,24 +1,42 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AudioList from "../screens/AudioList";
-import Player from "../screens/Player";
-import Playlist from "../screens/PlayList";
-import { Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import HomeScreen from "../screens/HomeScreen";
+import UploadScreen from "../screens/UploadScreen";
+import AccountScreen from "../screens/AccountScreen";
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="AudioList" component={AudioList} options={{
-          tabBarIcon: ({color, size}) => ( <Feather name="list" size={size} color={color} /> )
-      }}/>
-      <Tab.Screen name="Player" component={Player} options={{
-          tabBarIcon: ({color, size}) => ( <MaterialCommunityIcons name="disc-player" size={size} color={color} /> )
-      }}/>
-      <Tab.Screen name="PlayList" component={Playlist} options={{
-          tabBarIcon: ({color, size}) => ( <MaterialIcons name="playlist-play" size={size} color={color} /> )
-      }}/>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Upload"
+        component={UploadScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="upload" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
